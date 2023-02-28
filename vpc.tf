d3bb9cec388a243f6f4169a5ba4145d1745a2594
Tapan111-GitHub <126258346+Tapan111-GitHub@users.noreply.github.com>
2023-02-28 16:37:29 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,4 +20,16 @@ resource "aws_s3_bucket" "temps3" {
     bucket = "tapanbucket007" 
     acl = "private"   
 }
-
+resource "aws_db_instance" "rds_instance" {
+  allocated_storage = 20
+  identifier = "rds-terraform"
+  storage_type = "gp2"
+  engine = "mysql"
+  engine_version = "8.0.27"
+  instance_class = "db.t2.micro"
+  name = "mysqlrds-by-Tapan"
+  username = "admin"
+  password = "Calibo123#"
+  publicly_accessible    = true
+  skip_final_snapshot    = true  
+  tags = {    Name = "RDSServerInstance"  }}

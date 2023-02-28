566a6fd4e7693e38680b000e986cb77126943c43
Tapan111-GitHub <126258346+Tapan111-GitHub@users.noreply.github.com>
2023-02-28 17:15:42 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,4 +20,15 @@ resource "aws_s3_bucket" "temps3" {
     bucket = "tapanbucket007" 
     acl = "private"   
 }
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
 
